c260e9f4bd193a5c78d984d95f258a3200204e08
MauricioYuji-Code <mauricio.nakandakari2@gmail.com>
2021-04-15 22:54:09 -0300

    upd

---
+++ src/Gen/Neuronio.java
@@ -0,0 +1,33 @@
+package Gen;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Neuronio {
+
+    private float result;
+
+    //nivel de ativação
+    public void soma(List<Float> entradas, List<Float> pesos) {
+        float resultadoSoma = 0;
+        for (int i = 0 ; i<entradas.size() ; i++){
+            for (int j = 0 ; j<pesos.size() ; j++){
+                if(i == j){
+                    result = entradas.get(i) * entradas.get(j);
+                    System.out.println("Resultado Multiplicação" + result);
+                    resultadoSoma += result;
+                }
+            }
+        }
+        System.out.println("Resultado da soma " + resultadoSoma);
+    }
+
+    public void ativacao() {
+
+    }
+
+    public Float saida(float n) {
+        return n;
+    }
+}
--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,18 +1,36 @@
 package Main;
 
+import Gen.Neuronio;
 import Utils.Matriz;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Matriz matrizA = new Matriz(3, 2);
-        Matriz matrizB = new Matriz(2, 3);
-
+//        Matriz matrizA = new Matriz(2, 2);
+//        Matriz matrizB = new Matriz(2, 2);
+//
 //        Matriz.sumMatriz(matrizA, matrizB);
-        Matriz.multiplyMatriz(matrizA, matrizB);
+//        Matriz.multiplyMatriz(matrizA, matrizB);
+
+        Neuronio neuronio = new Neuronio();
+        List<Float>entradas = new ArrayList<>();
+        List<Float>pesos = new ArrayList<>();
+        entradas.add(3F);
+        entradas.add(3F);
+        pesos.add(3F);
+        pesos.add(3F);
+
+
+        neuronio.soma(entradas, pesos);
+
 
     }
+
+
 
 
 }
